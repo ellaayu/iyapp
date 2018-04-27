06184d336cec9662bcdfc68f0ec2bfce794d9e88
410-06\user <ella.ayuningtyas.eru@gmail.com>
2018-04-27 10:16:38 +0900

    8

---
+++ 8출력.cpp
@@ -0,0 +1,26 @@
+/*
+ * aaa.cpp
+ *
+ *  Created on: 2018. 4. 11.
+ *      Author: user
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int a=0; a<5;a++){
+		int c = 2;
+		int d = c*a;
+		printf("a*b = %d \n",d);
+		printf("a*b = %d \n", d);
+		
+	}
+//	int a = 2*1;
+//	printf("a-b = %d \n",a);
+		
+    cout << "!!!Hello World!!!" << endl;
+    return 0;
+}
+
+		
